264c61d6f4af1c6ed19bbbbc1ee83eed86754fc3
VGaming <andrewvichit@gmail.com>
2025-10-28 08:39:13 -0400

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -42,7 +42,7 @@ class Player(pyglet.sprite.Sprite):
 
     def end_attack(self, dt):
         self.is_attacking = False
-        self.image = pyglet.resource.image('images (1).jpg')
+        self.image = pyglet.resource.image()
         self.scale = 1
 
     def update(self, dt):
@@ -56,3 +56,4 @@ class Player(pyglet.sprite.Sprite):
             self.y -= 10
         if self.cooldown > 0:
             self.cooldown -= 1
+
