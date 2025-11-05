7dfcef9452a4405bd2388e69072393ce0b29c470
VGaming <andrewvichit@gmail.com>
2025-11-05 09:02:16 -0500

    Add files via upload

--- stickEnemy.py
+++ stickEnemy.py
@@ -7,19 +7,21 @@ def center_image(image):
     image.anchor_x = image.width // 2
     image.anchor_y = image.height // 2
 
-enemy_image = pyglet.resource.image(image_here)
+enemy_image = pyglet.resource.image('Basic_human_drawing.png')
 center_image(enemy_image)
 
 
 
-def spawn_Enemy(amount):
+
+def spawn_Enemy(amount, player_x, player_y):
     for i in range(amount):
-        enemy_x = random.randint(500, 2000)
+        enemy_x = random.randint(500, 1500)
         enemy_y = random.randint(200, 1200)
+        while (enemy_x <= (player_x + 500) and enemy_x >= (player_x - 500)) or (enemy_y <= (player_y + 500) and enemy_y >= (player_y - 500)):
+            enemy_x = random.randint(500, 1500)
+            enemy_y = random.randint(200, 1200)
         new_Enemy = pyglet.sprite.Sprite(enemy_image
                                         , enemy_x, enemy_y)
         enemies.append(new_Enemy)
     return enemies
-
-
 
